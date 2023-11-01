fedfcb514d8925d86909cbe21f6bd20a63ff7320
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-01 15:50:20 -0700

    Create friend request action methods

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -17,3 +17,47 @@ exports.create_friend_request = asyncHandler(async (req, res, next) => {
   await friendRequest.save();
   res.status(200).json('friend request sent');
 });
+
+exports.accept_friend_request = asyncHandler(async (req, res, next) => {
+  const requestId = req.body.id;
+  const friendRequest = await FriendRequest.findByIdAndDelete(requestId);
+  const [to, from] = await Promise.all([
+    User.findOne({ _id: friendRequest.to })
+      .populate('friendRequests')
+      .populate('friends').exec,
+    User.findOne({ _id: friendRequest.from })
+      .populate('friendRequests')
+      .populate('friends').exec
+  ]);
+  to.friends.push(from);
+  to.friendRequests = to.friendRequests.filter(
+    (request) => request.from !== from
+  );
+  from.friends.push(to);
+  from.friendRequests = from.friendRequests.filter(
+    (request) => request.from !== to
+  );
+  await to.save();
+  await from.save();
+});
+
+exports.decline_friend_request = asyncHandler(async (req, res, next) => {
+  const requestId = req.body.id;
+  const friendRequest = await FriendRequest.findByIdAndDelete(requestId);
+  const [to, from] = await Promise.all([
+    User.findOne({ _id: friendRequest.to })
+      .populate('friendRequests')
+      .populate('friends').exec,
+    User.findOne({ _id: friendRequest.from })
+      .populate('friendRequests')
+      .populate('friends').exec
+  ]);
+  to.friendRequests = to.friendRequests.filter(
+    (request) => request.from !== from
+  );
+  from.friendRequests = from.friendRequests.filter(
+    (request) => request.from !== to
+  );
+  await to.save();
+  await from.save();
+});
