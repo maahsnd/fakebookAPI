56ecc851fe72dbc77a03f3a16f1bf3a87a264910
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-01 14:47:47 -0700

    Create friend request model

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   username: { type: String, required: true },
   friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }]
+  posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+  friendRequests: [{ type: Schema.Types.ObjectId, ref: 'FriendRequest' }]
 });
 
 module.exports = mongoose.model('User', UserSchema);
