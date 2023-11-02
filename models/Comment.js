a0c71efc56db34c71126e3aca59c10d542268395
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-02 10:19:57 -0700

    Remove likes field Beyond the scope of initial implementation.

--- models/Comment.js
+++ models/Comment.js
@@ -6,7 +6,6 @@ const CommentSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: 'User' },
   text: { type: String, required: true },
   post: { type: Schema.Types.ObjectId, ref: 'Post' },
-  likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   time: { type: Date, default: Date.now() }
 });
 
