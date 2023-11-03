ae309fb4c3931352d82a169e91adcd78e8ccd613
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-03 09:58:12 -0700

    Remove auth middleware Not functioning, revisit later.

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/user-controller');
-const authHelper = require('../authHelper');
 
 /* GET users listing. */
-router.get('/:id', authHelper.checkAuth, UserController.get_user);
+router.get('/:id', UserController.get_user);
 
-router.post(
-  '/:id/friendrequests',
-  authHelper.checkAuth,
-  UserController.create_friend_request
-);
+router.post('/:id/friendrequests', UserController.create_friend_request);
 
 router.post(
   '/:id/friendrequests/:requestid/accept',
-  authHelper.checkAuth,
   UserController.accept_friend_request
 );
 
 router.post(
   '/:id/friendrequests/:requestid/decline',
-  authHelper.checkAuth,
   UserController.decline_friend_request
 );
 
