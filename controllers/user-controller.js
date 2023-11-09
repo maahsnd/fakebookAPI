8a1921da46c608687136162cfd55439ba6f3a8fc
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-08 17:48:24 -0800

    Repair friend request methods Previous methods did not work. Though friends could be added, the friend request field was not updated. Now done exclusively through mongo queries. Additionally, suggested friends route updated to not return users who have requested user as a friend.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,9 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/User');
 const { request } = require('https');
+const { ObjectId } = require('mongodb');
 
 exports.get_user = asyncHandler(async (req, res, next) => {
-  console.log('req.user--->' + req.user);
-  console.log('req.session.passport--->' + req.session.passport);
   const data = await User.findOne({ _id: req.params.id })
     .populate('friendRequests')
     .exec();
@@ -27,11 +26,17 @@ exports.get_friends = asyncHandler(async (req, res, next) => {
 exports.get_suggested_friends = asyncHandler(async (req, res, next) => {
   const userid = req.params.id;
   try {
+    const data = await User.findById(userid, {
+      _id: 0,
+      friendRequests: 1
+    }).exec();
+    const friendRequests = data.friendRequests;
     const users = await User.find({
       $and: [
         { friends: { $nin: [userid] } }, // Exclude users where userid is in their friends
         { _id: { $ne: userid } }, // Exclude users where ID matches userid,
-        { friendRequests: { $nin: [userid] } }
+        { friendRequests: { $nin: [userid] } }, //Exclude users who current user has already requested
+        { _id: { $nin: friendRequests } } //Exclude users who have already requested current user
       ]
     })
       .sort('username')
@@ -47,53 +52,39 @@ exports.create_friend_request = asyncHandler(async (req, res, next) => {
   const from = req.params.id;
   const to = req.body.to;
   await User.findByIdAndUpdate(to, { $push: { friendRequests: from } });
-
   res.status(200).json({ to: to, from: from });
 });
 
 exports.accept_friend_request = asyncHandler(async (req, res, next) => {
   const requestId = req.params.requestid;
   const userId = req.params.id;
-  const [to, from] = await Promise.all([
-    User.findOne({ _id: userId })
-      .populate('friendRequests')
-      .populate('friends')
-      .exec(),
-    User.findOne({ _id: requestId })
-      .populate('friendRequests')
-      .populate('friends')
-      .exec()
-  ]);
-  to.friends.push(requestId);
-  to.friendRequests = to.friendRequests.filter(
-    (request) => request !== requestId
-  );
-  from.friends.push(userId);
-  await to.save();
-  await from.save();
-  res.status(200).json({ to: to, from: from });
+  try {
+    await Promise.all([
+      User.findOneAndUpdate(
+        { _id: userId },
+        { $push: { friends: requestId }, $pull: { friendRequests: requestId } }
+      ).exec(),
+      User.findOneAndUpdate({ _id: requestId }, { $push: { friends: userId } })
+    ]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+    return;
+  }
+  res.status(200).send();
 });
 
 exports.decline_friend_request = asyncHandler(async (req, res, next) => {
   const requestId = req.params.requestid;
   const userId = req.params.id;
-  const [to, from] = await Promise.all([
-    User.findOne({ _id: userId })
-      .populate('friendRequests')
-      .populate('friends')
-      .exec(),
-    User.findOne({ _id: requestId })
-      .populate('friendRequests')
-      .populate('friends')
-      .exec()
-  ]);
-  to.friendRequests = to.friendRequests.filter(
-    (request) => request !== from._id
-  );
-  from.friendRequests = from.friendRequests.filter(
-    (request) => request !== to._id
-  );
-  await to.save();
-  await from.save();
-  res.status(200).json({ to: to, from: from });
+  try {
+    await User.findOneAndUpdate(
+      { _id: userId },
+      { $pull: { friendRequests: requestId } }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send();
+  }
+  res.status(200).send();
 });
