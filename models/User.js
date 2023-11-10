a6ae9a8b691736be9473dd29c95453a454350091
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-10 15:56:25 -0800

    Add password field

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   username: { type: String, required: true },
+  password: { type: String, required: true },
   friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
   friendRequests: [{ type: Schema.Types.ObjectId, ref: 'User' }],
