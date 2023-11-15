002c8d816617f10d0141f1bfe864a557ed38fc48
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-15 09:59:04 -0800

    Middleware not effective, remove

--- app.js
+++ app.js
@@ -12,14 +12,6 @@ require('./mongoConfig');
 
 const app = express();
 app.use(cors());
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
 
 const RateLimit = require('express-rate-limit');
 const limiter = RateLimit({
