9ea83601c3558b57a07cfb345a0e36e240ce5403
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-15 10:15:36 -0800

    Remove custom middleware, restore cors Solution unsuccessful.

--- app.js
+++ app.js
@@ -11,15 +11,7 @@ require('dotenv').config();
 require('./mongoConfig');
 
 const app = express();
-/* app.use(cors()); */
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+app.use(cors());
 
 const RateLimit = require('express-rate-limit');
 const limiter = RateLimit({
