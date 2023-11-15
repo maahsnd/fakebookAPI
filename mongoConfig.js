6199077f87bc7b25b7792dd36f6b857395b8d21e
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-14 17:32:39 -0800

    Try moving mongo string to railway var

--- mongoConfig.js
+++ mongoConfig.js
@@ -1,7 +1,6 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect(process.env.MONGO_STRING);
+  await mongoose.connect(MONGO_STRING);
 }
