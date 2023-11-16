5f3d61c38f906b75983d8427a455866ceae858dd
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-16 13:10:19 -0800

    Remove unused variable, typo

--- mongoConfig.js
+++ mongoConfig.js
@@ -3,9 +3,9 @@ mongoose.set('strictQuery', false);
 main().catch((err) => console.log(err));
 async function main() {
   try {
-    let connection = await mongoose.connect(process.env.MONGO_STRING);
+    await mongoose.connect(process.env.MONGO_STRING);
     console.log(`Successfully connected to MONGO`)  
   } catch (error) {
-    console.errorR(error)
+    console.error(error)
   }
 }
