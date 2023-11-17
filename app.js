b837f350f7e1dceea65ded909071dd3498d27031
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-16 18:53:02 -0800

    Set limits

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ app.use(
 app.set('trust proxy', 1);
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50 mb'}));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
